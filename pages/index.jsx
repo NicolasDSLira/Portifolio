779e38e8bb4f51ef6d713ba70391c248ec5a2a88
Nicolas dos Santos Lira <80727008+NicolasDSLira@users.noreply.github.com>
2022-04-21 18:32:56 -0300

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,87 @@
+import Head from 'next/head'
+import Style from '../styles/index.module.css'
+import Image from 'next/image'
+
+
+const Home = ({projetos}) => {
+
+    let name = "Nicolas dos Santos Lira"
+
+
+    return(
+        <>
+        
+            <Head>
+                <meta charset='UTF-8'/>
+                <title>{name}</title>
+            </Head>
+
+            <main>
+
+                <div className={Style.header}>
+                    <div className={Style.conteiner}>
+                        <Image
+                            priority 
+                            src={"/image/me.jpg"}
+                            height={150}
+                            width={150}
+                            className={Style.meImage}
+                        />
+                        <h1 className={Style.titleHeader}>{name}</h1>
+                        <p className={Style.descHeader}>Desenvolvedor Full-Stack Júnior</p>
+                    </div>
+
+                    <div className={Style.animateArrow}>
+                        <p className={Style.textArrow}>Projetos</p>
+                        <i className={Style.arrow}></i>
+                    </div>
+
+                </div>
+
+                <section className={Style.projetos}>
+
+            
+                    
+                    {projetos.map((projeto) => (
+                        <div className={Style.itemProjetos} key={projeto.Id}>
+                            <Image
+                                priority
+                                src={'/image/projetos/'+ projeto.Imagem +'.PNG'}
+                                width={455}
+                                height={167.666666}
+                            />
+
+                            <div className={Style.colProjeto}>
+                                <h1>{projeto.Titulo}</h1>
+                                <p><b>Descrição: {' '}</b> {projeto.Descricao}</p>
+                                <hr/>
+                                <p><b>Tecnologias utilizadas:</b> {' ' + projeto.LT + '.'}</p>
+
+                            </div>
+                        </div>
+                    ))}
+        
+
+                </section>
+
+            </main>
+
+
+        </>
+    
+    )
+}
+
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://nicolasdslira.github.io/Projetos/')
+    const projetos = await res.json()
+
+    return{
+        props: {
+            projetos,
+        },
+    }
+}
+
+export default Home
